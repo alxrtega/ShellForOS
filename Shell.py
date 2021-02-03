342325694bd3660cca0e32913810c7c3d57399ff
alejandro <a.ortega6243@gmail.com>
2021-02-03 09:57:49 -0700

    I will be using os.read and os.write

--- Shell.py
+++ Shell.py
@@ -9,22 +9,19 @@ class Shell:
         self.prompt()
 
     def prompt(self):
-
         while 1:
-            userInput = self.pathLine()
-
-            os.write(1, (userInput + "\n").encode())
-            if userInput == "exit":
+            terminal = self.pathLine()
+            os.write(1, (terminal + "\n").encode())
+            if terminal == "exit":
                 sys.exit(0)
 
     def pathLine(self):
-        message = os.getcwd() + "\n$ "
-        os.write(2, (message).encode())
+        terminal = os.getcwd() + "\n$ "
+        os.write(2, (terminal).encode())
         userInput = os.read(0, 128)
         userInput = userInput.decode().split("\n")
         userInput = userInput[0]
         userInput = str(userInput)
-
         return userInput
 
 shell = Shell()
